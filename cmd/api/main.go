1413c6231efd58efdecf51eaa2c03c3650c752e6
eegurt <alihan28.06.0450@gmail.com>
2023-02-22 15:03:49 +0600

    json log

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -13,6 +12,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"godvanced.forstes.github.com/internal/data"
+	"godvanced.forstes.github.com/internal/jsonlog"
 )
 
 const version = "1.0.0"
@@ -29,17 +29,17 @@ type config struct {
 }
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
 func main() {
 	var cfg config
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	err := godotenv.Load(".env.local")
 	if err != nil {
-		logger.Fatal("Error loading .env file")
+		logger.PrintFatal(err, nil)
 	}
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
@@ -53,15 +53,15 @@ func main() {
 
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 
 	defer db.Close()
-	logger.Printf("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	expireHours, err := strconv.Atoi(os.Getenv("JWT_EXPIRY_HOURS"))
 	if err != nil {
-		logger.Fatal("Couldn't convert expiry string to int")
+		logger.PrintFatal(err, nil)
 		return
 	}
 	cfg.jwtOptions = &jwtOptions{key: os.Getenv("JWT_KEY"), expires: time.Duration(expireHours) * time.Hour}
@@ -80,9 +80,12 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  cfg.env,
+	})
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*pgxpool.Pool, error) {
